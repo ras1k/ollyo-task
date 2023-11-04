b268f45a35bf5ab6e438d3b752032d5f5741e874
Rasik Abdullah <rasikabdullah@gmail.com>
2023-11-04 19:36:00 +0600

    update code

--- src/components/dragDrop.jsx
+++ src/components/dragDrop.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import { useRef, useState } from "react";
+import img1 from '../assets/images/images (1).jpeg'
+import img2 from '../assets/images/images (2).jpeg'
+import img3 from '../assets/images/images (3).jpeg'
+import img4 from '../assets/images/images (4).jpeg'
+import img5 from '../assets/images/images (5).jpeg'
+import img6 from '../assets/images/images (6).jpeg'
+import img7 from '../assets/images/images (7).jpeg'
+import img8 from '../assets/images/images (8).jpeg'
+import img9 from '../assets/images/images (9).jpeg'
+import img10 from '../assets/images/images (10).jpeg'
+import img11 from '../assets/images/images (11).jpeg'
 
-const dragDrop = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default dragDrop;
+const imagesData = [
+    {
+        id: 1,
+        src: img1,
+        isFeature: true,
+    },
+    {
+        id: 2,
+        src: img2,
+        isFeature: false,
+    },
+    {
+        id: 3,
+        src: img3,
+        isFeature: false,
+    },
+    {
+        id: 4,
+        src: img4,
+        isFeature: false,
+    },
+    {
+        id: 5,
+        src: img5,
+        isFeature: false,
+    },
+    {
+        id: 6,
+        src: img6,
+        isFeature: false,
+    },
+    {
+        id: 7,
+        src: img7,
+        isFeature: false,
+    },
+    {
+        id: 8,
+        src: img8,
+        isFeature: false,
+    },
+    {
+        id: 9,
+        src: img9,
+        isFeature: false,
+    },
+    {
+        id: 10,
+        src: img10,
+        isFeature: false,
+    },
+    {
+        id: 11,
+        src: img11,
+        isFeature: false,
+    },
+];
